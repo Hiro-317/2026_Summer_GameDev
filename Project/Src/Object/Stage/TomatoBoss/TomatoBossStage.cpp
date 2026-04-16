c31a61f7bcf3adece953324e74ec1659ebb68a22
ASOST\2416082 <tdashu133@gmail.com>
2026-04-16 17:51:52 +0900

    クラス定義

---
+++ Project/Src/Object/Stage/TomatoBoss/TomatoBossStage.cpp
@@ -0,0 +1,9 @@
+#include "TomatoBossStage.h"
+
+TomatoBossStage::TomatoBossStage()
+{
+}
+
+TomatoBossStage::~TomatoBossStage()
+{
+}
--- Project/Src/Object/Stage/TomatoBoss/TomatoBossStage.h
+++ Project/Src/Object/Stage/TomatoBoss/TomatoBossStage.h
@@ -1,1 +1,11 @@
 #pragma once
+
+class TomatoBossStage
+{
+public:
+	TomatoBossStage();
+	~TomatoBossStage();
+
+private:
+
+};
