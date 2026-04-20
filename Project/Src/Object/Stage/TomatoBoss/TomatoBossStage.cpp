607f0ec7870e5872b1925197ad99926251d17574
ASOST\2416082 <tdashu133@gmail.com>
2026-04-20 15:58:57 +0900

    当たり判定の追加

--- Project/Src/Object/Stage/TomatoBoss/TomatoBossStage.cpp
+++ Project/Src/Object/Stage/TomatoBoss/TomatoBossStage.cpp
@@ -2,8 +2,10 @@
 
 #include"../../Common/Collider/BoxCollider.h"
 #include"../../Common/Collider/CapsuleCollider.h"
+#include"../../Common/Collider/SphereCollider.h"
 
-TomatoBossStage::TomatoBossStage() 
+TomatoBossStage::TomatoBossStage() :
+	ActorBase("Data/Parameter/Stage/TomatoStageParameter.csv")
 {
 }
 
@@ -12,8 +14,11 @@ void TomatoBossStage::Load(void)
 	trans.Load("Stage/TomatoBoss/TomatoBossStage");
 
 	trans.centerDiff = MODEL_CENTER_DIFF;
-
+	
+	// 衝突判定の情報読み込み
 	ColliderLoad();
+
+	trans.angle.y = Deg2Rad(45.0f);
 }
 
 void TomatoBossStage::ColliderLoad()
@@ -21,10 +26,13 @@ void TomatoBossStage::ColliderLoad()
 	// 地面の当たり判定
 	ColliderCreate(new BoxCollider(TAG::STAGE, GROUND_COLLISION_SIZE));
 
+	for (const ColliderInfo& info : WALL_COLLISION_INFO) {
+		ColliderCreate(new CapsuleCollider(TAG::STAGE, Vector3(0.0f), Vector3::Yonly(300.0f), info.radius, -1.0f, info.pos));
+	}
+
 	// ステージの岩の当たり判定
-	for (int i = 0; i < ROCK_NUM; i++)
-	{
-		ColliderCreate(new CapsuleCollider(TAG::STAGE, Vector3(0.0f), Vector3::Yonly(100.0f), ROCK_COLLISION_INFO[i].radius, -1.0f, ROCK_COLLISION_INFO[i].pos));
+	for (const ColliderInfo& info : ROCK_COLLISION_INFO) {
+		ColliderCreate(new CapsuleCollider(TAG::STAGE, Vector3(0.0f), Vector3::Yonly(100.0f), info.radius, -1.0f, info.pos));
 	}
 }
 
